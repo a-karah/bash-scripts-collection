a522b94130f0861244480e8103c6a718f3c3bbcd
akarah <akarahan096@gmail.com>
2022-12-20 21:44:36 +0300

    improve script, now works with bash :)

--- prepare-c-files-for-makefile.sh
+++ prepare-c-files-for-makefile.sh
@@ -1,8 +1,15 @@
-# WTF! Only works with sh
+#!/bin/bash
+# https://superuser.com/questions/951905/is-there-a-way-to-echo-a-blackslash-followed-by-newline-in-bash
 
-#!/bin/sh
-
-echo "SRCS = \\"
-for i in *.c; do
-	echo "\t$i \\"
+i=1
+SRCS="SRCS = "
+for file in *.c; do
+	if [ $i -eq 1 ]; then
+		SRCS+="${file} \\\\\n"
+	else
+		SRCS+="\t${file} \\\\\n"
+	fi
+	((i++))
 done
+
+echo -e $SRCS
